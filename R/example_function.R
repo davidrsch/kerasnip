35ecc8ff7d26f0be7d5b0846c8cef0edd1c011d0
davidrsch <daviddrsch@gmail.com>
2024-08-26 14:04:18 +0200

    Adding an example_function

---
+++ R/example_function.R
@@ -0,0 +1,15 @@
+#' Sum two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#'
+#' @return The sum of both numbers.
+#' @export
+#'
+#' @examples
+#' x <- 1
+#' y <- 2
+#' example_function(x, y)
+example_function <- function(x, y) {
+  x + y
+}
